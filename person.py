94420a5e2f385e185e86c39f47195b78a6806ab0
eileen-nkust <c110225132@nkust.edu.tw>
2026-08-03 13:10:12 +0800

    Implement Person class with methods for name and age

--- person.py
+++ person.py
@@ -1,10 +1,10 @@
-# class Person:
-#     def __init__(self,name,age):
-#         self.name = name
-#         self.age = age
+class Person:
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
 
-#     def print_name(self):
-#         print(self.name)
+    def print_name(self):
+        print(self.name)
 
-#     def print_age(self):
-#         print(self.age)
+    def print_age(self):
+        print(self.age)
